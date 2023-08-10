b78b3c8b9d9f042a070616ff6e9eab5d1deca04f
Paul Heller <paulleonardoheller@yahoo.de>
2023-08-10 10:29:06 +0200

    added early stopping

--- MNIST-DiffusionModel/trainer.py
+++ MNIST-DiffusionModel/trainer.py
@@ -23,7 +23,9 @@ class Trainer:
         loss_show_epoch: int = 1,
         sample_epoch: int = 2,
         save_epoch: int = 1,
-        dataloader: DataLoader = None,
+        early_stopping: bool = False,
+        train_dataloader: DataLoader = None,
+        val_dataloader: DataLoader = None,
         device: str = "cpu",
     ):
         self.diffusion_model = diffusion_model
@@ -37,13 +39,25 @@ class Trainer:
         self.loss_show_epoch = loss_show_epoch
         self.sample_epoch = sample_epoch
         self.save_epoch = save_epoch
+        self.early_stopping = early_stopping
         self.device = device
 
         # no parameters in constructor
         self.start_epoch = 0
-        self.dataloader = create_mnist_dataloaders(
-            batch_size=self.batch_size, image_size=self.image_size, num_workers=4
-        )
+
+        # early stopping regarded:
+        self.patience = 10
+        self.best_val_loss = float("inf")
+        self.patience_counter = 0
+        self.val_dataloader = val_dataloader
+
+        if not early_stopping:
+            self.train_dataloader = create_mnist_dataloaders(
+                batch_size=self.batch_size, image_size=self.image_size, num_workers=4
+            )
+        else:
+            self.train_dataloader = train_dataloader
+
         if self.use_ema:
             self.ema = EMA(beta=0.995)
             self.ema_model = (
@@ -57,12 +71,12 @@ class Trainer:
             epoch_plus1 = epoch + 1
             self.diffusion_model.model.train()
             print(f"Epoch: {epoch_plus1}")
-            pbar = tqdm(self.dataloader, desc="Training Loop")
+            pbar = tqdm(self.train_dataloader, desc="Training Loop")
 
             for step, batch in enumerate(pbar):
                 x, y = batch
-                x = x.type(torch.float32).to(self.device)
-                y = y.type(torch.long).to(self.device)  # contains 0
+                # x = x.type(torch.float32).to(self.device)
+                # y = y.type(torch.long).to(self.device)  # contains 0
 
                 # Algorithm 1 line 3: sample t uniformally for every example in the batch
                 # sampling a t to generate t and t+1
@@ -80,13 +94,34 @@ class Trainer:
 
                 training_loss.append(loss.item())
 
+                # early stopping:
+                if step == 700:
+                    break
+
+                if self.early_stopping:
+                    val_loss = self.compute_validation_loss()
+
+                    # Check for improvement
+                    if val_loss < self.best_val_loss:
+                        self.best_val_loss = val_loss
+                        self.patience_counter = 0
+                    else:
+                        self.patience_counter += 1
+
+                    # If patience is exceeded, stop the training
+                    if self.patience_counter >= self.patience:
+                        print("Early stopping due to no improvement.")
+                        break
+
             # Saving model
             if (epoch_plus1) % self.save_epoch == 0 or (epoch_plus1) == self.nb_epochs:
                 # epoch + 1
                 self.save_model(epoch_plus1)
 
             # Plot and Save loss figure
-            if (epoch_plus1 % self.loss_show_epoch == 0) or (epoch_plus1 == self.nb_epochs):
+            if (epoch_plus1 % self.loss_show_epoch == 0) or (
+                epoch_plus1 == self.nb_epochs
+            ):
                 print(f"Epoch {epoch_plus1} Loss:", training_loss[-1])
                 plt.plot(training_loss)
                 plt.title("Training loss")
@@ -94,11 +129,37 @@ class Trainer:
                 plt.close()
 
             # save generated images
-            if (epoch_plus1 % self.sample_epoch == 0) or (epoch_plus1 == self.nb_epochs):
-                self.sampling(epoch_plus1, cond_weight=self.cond_weight, sample_random=False)
+            if (epoch_plus1 % self.sample_epoch == 0) or (
+                epoch_plus1 == self.nb_epochs
+            ):
+                self.sampling(
+                    epoch_plus1, cond_weight=self.cond_weight, sample_random=False
+                )
+
+    def compute_validation_loss(self):
+        # Compute validation loss using your validation data loader
+        # Ensure you switch the model to eval mode and use torch.no_grad()
+        # You will compute loss similar to your training loop, but without any backward pass or optimizer step
+        total_loss = 0
+        self.diffusion_model.model.eval()
+        with torch.no_grad():
+            for batch in self.val_dataloader:
+                x, y = batch
+                
+                if self.use_guided_diff:
+                    loss = self.diffusion_model(x=x, classes=y, p_uncond=0.1)
+                else:
+                    loss = self.diffusion_model(x=x, classes=None, p_uncond=0)
+                total_loss += loss.item()
+        avg_loss = total_loss / len(self.val_dataloader)
+        return avg_loss
 
     def sampling(
-        self, epoch: int, n_samples: int = 20, cond_weight: float = 2.0, sample_random: bool=False
+        self,
+        epoch: int,
+        n_samples: int = 20,
+        cond_weight: float = 2.0,
+        sample_random: bool = False,
     ) -> None:
         """
 
@@ -112,10 +173,12 @@ class Trainer:
             if sample_random:
                 classes_list = np.arange(0, 10)
                 classes = torch.from_numpy(np.random.choice(classes_list, n_samples))
-            #classes = sampled.cuda()
+            # classes = sampled.cuda()
             else:
-                classes = torch.arange(0,10).to('cpu') # context for us just cycles throught the mnist labels
-                classes = classes.repeat(int(20/classes.shape[0]))
+                classes = torch.arange(0, 10).to(
+                    "cpu"
+                )  # context for us just cycles throught the mnist labels
+                classes = classes.repeat(int(20 / classes.shape[0]))
             print("We sample the following classes: ")
             print(classes)
 
@@ -128,7 +191,7 @@ class Trainer:
             )
 
         plt.figure(figsize=(16, 16))
-        #int_s2root = int(np.sqrt(n_samples))
+        # int_s2root = int(np.sqrt(n_samples))
         for i in range(n_samples):
             plt.subplot(5, 4, 1 + i)
             plt.axis("off")
