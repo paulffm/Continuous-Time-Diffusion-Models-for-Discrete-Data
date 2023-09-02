47803e395df5592fe5097c3acee8e3c6c497ea26
Paul Heller <paulleonardoheller@yahoo.de>
2023-09-02 15:19:38 +0200

    added dataset

---
+++ DiscreteTimeDiscreteSpace/datasets.py
@@ -0,0 +1,40 @@
+import torch
+import torchvision.transforms as T
+import torchvision.datasets
+import numpy as np
+from torch.utils.data import Subset
+TRAINSUBSET = 0
+
+
+
+def get_train_data(conf):
+    if conf.dataset.name == 'cifar10':
+        transform = T.Compose(
+            [
+                T.ToTensor(),
+                lambda x: x * 255
+            ]
+        )
+        transform_test = T.Compose(
+            [
+                T.ToTensor(),
+                lambda x: x * 255
+            ]
+        )
+
+        train_set = torchvision.datasets.MNIST(conf.dataset.path,
+                                                 train=True,
+                                                 transform=transform,
+                                                 download=True)
+        eval_set = torchvision.datasets.MNIST(conf.dataset.path,
+                                                  train=False,
+                                                  transform=transform_test,
+                                                  download=True)
+
+        if TRAINSUBSET:
+            # limit_size = list(range(min(len(train_set), conf.training.dataloader.batch_size*10+1000)))
+            limit_size = list(range(128))
+            train_set = Subset(train_set, limit_size)
+            eval_set = Subset(eval_set, limit_size)
+
+    return train_set, eval_set
