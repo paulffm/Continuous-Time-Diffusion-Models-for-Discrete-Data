e63639e579554340e97abebafc94572abb42c8d1
Paul Heller <paulleonardoheller@yahoo.de>
2023-09-04 17:51:54 +0200

    own saving, loading func

--- ContTimeDiscreteSpace/tauLDR/lib/utils/bookkeeping.py
+++ ContTimeDiscreteSpace/tauLDR/lib/utils/bookkeeping.py
@@ -9,14 +9,16 @@ import glob
 import os
 import numpy as np
 import sys
+import ruamel.yaml
 import signal
+from ml_collections.config_dict import config_dict
 
 
 def create_experiment_folder(save_location, inner_folder_name, include_time=True):
-    today_date = datetime.today().strftime(r'%Y-%m-%d')
-    now_time = datetime.now().strftime(r'%H-%M-%S')
+    today_date = datetime.today().strftime(r"%Y-%m-%d")
+    now_time = datetime.now().strftime(r"%H-%M-%S")
     if include_time:
-        total_inner_folder_name = now_time + '_' + inner_folder_name
+        total_inner_folder_name = now_time + "_" + inner_folder_name
     else:
         total_inner_folder_name = inner_folder_name
     path = Path(save_location).joinpath(today_date).joinpath(total_inner_folder_name)
@@ -26,114 +28,138 @@ def create_experiment_folder(save_location, inner_folder_name, include_time=True
 
     return path, checkpoint_dir, config_dir
 
-def create_inner_experiment_folders(path):
 
-    checkpoint_dir = path.joinpath('checkpoints')
+def create_inner_experiment_folders(path):
+    checkpoint_dir = path.joinpath("checkpoints")
     checkpoint_dir.mkdir(exist_ok=True)
 
-    checkpoint_archive_dir = checkpoint_dir.joinpath('archive')
+    checkpoint_archive_dir = checkpoint_dir.joinpath("archive")
     checkpoint_archive_dir.mkdir(exist_ok=True)
 
-    config_dir = path.joinpath('config')
+    config_dir = path.joinpath("config")
     config_dir.mkdir(exist_ok=True)
 
     return checkpoint_dir, config_dir
 
 
 def save_config_as_yaml(cfg, save_dir):
-    existing_configs = sorted(glob.glob(Path(save_dir).joinpath('config_*.yaml').as_posix()))
+    existing_configs = sorted(
+        glob.glob(Path(save_dir).joinpath("config_*.yaml").as_posix())
+    )
 
     if len(existing_configs) == 0:
-        save_name = Path(save_dir).joinpath('config_001.yaml')
+        save_name = Path(save_dir).joinpath("config_001.yaml")
     else:
         most_recent_config = existing_configs[-1]
         most_recent_num = int(most_recent_config[-8:-5])
-        save_name = Path(save_dir).joinpath('config_{0:03d}.yaml'.format(most_recent_num+1))
+        save_name = Path(save_dir).joinpath(
+            "config_{0:03d}.yaml".format(most_recent_num + 1)
+        )
 
-    with open(save_name, 'w') as f:
+    with open(save_name, "w") as f:
         yaml.dump(cfg.to_dict(), f)
+
 
 def save_git_hash(save_dir):
     git_hash = subprocess.check_output(["git", "rev-parse", "--verify", "HEAD"])
     git_hash = git_hash.decode("utf-8")
-    save_name = Path(save_dir).joinpath('git_hash.txt')
-    with open(save_name, 'w') as f:
+    save_name = Path(save_dir).joinpath("git_hash.txt")
+    with open(save_name, "w") as f:
         f.write(git_hash)
+
 
 def setup_tensorboard(save_dir, rank):
     if rank == 0:
-        logs_dir = Path(save_dir).joinpath('tensorboard')
+        logs_dir = Path(save_dir).joinpath("tensorboard")
         logs_dir.mkdir(exist_ok=True)
 
         writer = tensorboard.writer.SummaryWriter(logs_dir)
 
         return writer
     else:
-        return DummyWriter('none')
+        return DummyWriter("none")
+
 
 def save_checkpoint(checkpoint_dir, state, num_checkpoints_to_keep):
     state_to_save = {
-        'model': state['model'].state_dict(),
-        'optimizer': state['optimizer'].state_dict(),
-        'n_iter': state['n_iter']
+        "model": state["model"].state_dict(),
+        "optimizer": state["optimizer"].state_dict(),
+        "n_iter": state["n_iter"],
     }
-    torch.save(state_to_save,
-        checkpoint_dir.joinpath('ckpt_{0:010d}.pt'.format(state['n_iter']))
+    torch.save(
+        state_to_save,
+        checkpoint_dir.joinpath("ckpt_{0:010d}.pt".format(state["n_iter"])),
     )
-    all_ckpts = sorted(glob.glob(checkpoint_dir.joinpath('ckpt_*.pt').as_posix()))
+    all_ckpts = sorted(glob.glob(checkpoint_dir.joinpath("ckpt_*.pt").as_posix()))
     if len(all_ckpts) > num_checkpoints_to_keep:
         for i in range(0, len(all_ckpts) - num_checkpoints_to_keep):
             os.remove(all_ckpts[i])
 
+
 def save_archive_checkpoint(checkpoint_dir, state):
-    save_checkpoint(checkpoint_dir.joinpath('archive'), state, 9999999)
+    save_checkpoint(checkpoint_dir.joinpath("archive"), state, 9999999)
+
 
 def resume_training(resume_path, state, mapping):
-
-
     loaded_state = get_most_recent_checkpoint(resume_path, mapping)
 
-    state['model'].load_state_dict(loaded_state['model'])
-    state['optimizer'].load_state_dict(loaded_state['optimizer'])
-    state['n_iter'] = loaded_state['n_iter']
+    state["model"].load_state_dict(loaded_state["model"])
+    state["optimizer"].load_state_dict(loaded_state["optimizer"])
+    state["n_iter"] = loaded_state["n_iter"]
 
     return state
 
+
 def get_most_recent_checkpoint(dir, mapping):
     # Get most recent checkpoint
-    all_ckpts = sorted(glob.glob(dir.joinpath('checkpoints').joinpath('ckpt_*.pt').as_posix()))
+    all_ckpts = sorted(
+        glob.glob(dir.joinpath("checkpoints").joinpath("ckpt_*.pt").as_posix())
+    )
     most_recent_checkpoint = all_ckpts[-1]
 
     loaded_state = torch.load(most_recent_checkpoint, map_location=mapping)
 
     return loaded_state
 
+
 def load_ml_collections(path):
-    with open(path, 'r') as f:
+    with open(path, "r") as f:
         raw_dict = yaml.safe_load(f)
     return ml_collections.ConfigDict(raw_dict)
 
+
 def setup_eval_folders(experiment_path, eval_name, job_id=0, task_id=0):
-    eval_folder = experiment_path.joinpath('eval')
+    eval_folder = experiment_path.joinpath("eval")
     eval_folder.mkdir(exist_ok=True)
 
-    today_date = datetime.today().strftime(r'%Y-%m-%d')
-    now_time = datetime.now().strftime(r'%H-%M-%S')
+    today_date = datetime.today().strftime(r"%Y-%m-%d")
+    now_time = datetime.now().strftime(r"%H-%M-%S")
 
-    eval_folder_name_string = today_date + '_' + now_time + '_' + 'IvI' + eval_name + 'IvI' + f'_{job_id}_{task_id}'
+    eval_folder_name_string = (
+        today_date
+        + "_"
+        + now_time
+        + "_"
+        + "IvI"
+        + eval_name
+        + "IvI"
+        + f"_{job_id}_{task_id}"
+    )
     eval_named_folder = eval_folder.joinpath(eval_folder_name_string)
     eval_named_folder.mkdir(exist_ok=False)
-    eval_named_folder_configs = eval_named_folder.joinpath('config')
+    eval_named_folder_configs = eval_named_folder.joinpath("config")
     eval_named_folder_configs.mkdir(exist_ok=False)
 
     return eval_folder, eval_named_folder, eval_named_folder_configs
 
+
 def get_most_recent_config(config_path):
-    configs = sorted(glob.glob(config_path.joinpath('config_*.yaml').as_posix()))
+    configs = sorted(glob.glob(config_path.joinpath("config_*.yaml").as_posix()))
     assert len(configs) > 0
     return configs[-1]
 
-class NumpyWriter():
+
+class NumpyWriter:
     def __init__(self, save_dir):
         self.save_dir = save_dir
         self.scalar_data = {}
@@ -152,7 +178,7 @@ class NumpyWriter():
 
     def add_numpy_data(self, name, nparray, idx):
         self.numpy_data.append((name, nparray, idx))
-        
+
     def save_to_disk(self):
         for name in self.scalar_data:
             np_arr = np.zeros((len(self.scalar_data[name][0]), 2))
@@ -161,16 +187,19 @@ class NumpyWriter():
             np.savetxt(Path(self.save_dir).joinpath(name), np_arr)
         for item in self.figure_data:
             name, fig, idx = item
-            fig.savefig(Path(self.save_dir).joinpath(name + '_' + str(idx) + '.pdf'),
-                format='pdf')
+            fig.savefig(
+                Path(self.save_dir).joinpath(name + "_" + str(idx) + ".pdf"),
+                format="pdf",
+            )
         for item in self.numpy_data:
             name, nparray, idx = item
-            np.save(Path(self.save_dir).joinpath(name + '_' + str(idx)), nparray)
+            np.save(Path(self.save_dir).joinpath(name + "_" + str(idx)), nparray)
 
     def close(self):
         pass
 
-class DummyWriter():
+
+class DummyWriter:
     def __init__(self, save_dir):
         pass
 
@@ -184,15 +213,17 @@ class DummyWriter():
         pass
 
 
-def signal_handler(sig, frame, preemption_log_path, checkpoint_dir, state, num_checkpoints_to_keep):
-    time_string = datetime.today().strftime(r'%Y-%m-%d:%H-%M-%S')
+def signal_handler(
+    sig, frame, preemption_log_path, checkpoint_dir, state, num_checkpoints_to_keep
+):
+    time_string = datetime.today().strftime(r"%Y-%m-%d:%H-%M-%S")
     if sig == signal.SIGCONT:
         write_string = time_string + "  SIGCONT signal\n"
     elif sig == signal.SIGINT:
         write_string = time_string + "  SIGINT signal\n"
     elif sig == signal.SIGTERM:
         write_string = time_string + "  SIGTERM signal\n"
-    with open(preemption_log_path, 'a') as f:
+    with open(preemption_log_path, "a") as f:
         f.write(write_string)
 
     save_checkpoint(checkpoint_dir, state, num_checkpoints_to_keep)
@@ -200,65 +231,95 @@ def signal_handler(sig, frame, preemption_log_path, checkpoint_dir, state, num_c
     sys.exit(0)
 
 
-def setup_preemption(save_dir, checkpoint_dir, state, num_checkpoints_to_keep,
-    prepare_to_resume_after_timeout):
-
-    preemption_log_path = save_dir.joinpath('preemption_log.txt')
-    time_string = datetime.today().strftime(r'%Y-%m-%d:%H-%M-%S')
+def setup_preemption(
+    save_dir,
+    checkpoint_dir,
+    state,
+    num_checkpoints_to_keep,
+    prepare_to_resume_after_timeout,
+):
+    preemption_log_path = save_dir.joinpath("preemption_log.txt")
+    time_string = datetime.today().strftime(r"%Y-%m-%d:%H-%M-%S")
 
     if not os.path.isfile(preemption_log_path):
-        with open(preemption_log_path, 'w') as f:
-            f.write(time_string + '  start of log\n')
+        with open(preemption_log_path, "w") as f:
+            f.write(time_string + "  start of log\n")
             if prepare_to_resume_after_timeout:
-                f.write(time_string + ' Expecting Timeout\n')
+                f.write(time_string + " Expecting Timeout\n")
 
     signal.signal(
         signal.SIGCONT,
-        lambda sig, frame: signal_handler(sig, frame, preemption_log_path,
-            checkpoint_dir, state, num_checkpoints_to_keep)
+        lambda sig, frame: signal_handler(
+            sig,
+            frame,
+            preemption_log_path,
+            checkpoint_dir,
+            state,
+            num_checkpoints_to_keep,
+        ),
     )
     signal.signal(
         signal.SIGINT,
-        lambda sig, frame: signal_handler(sig, frame, preemption_log_path,
-            checkpoint_dir, state, num_checkpoints_to_keep)
+        lambda sig, frame: signal_handler(
+            sig,
+            frame,
+            preemption_log_path,
+            checkpoint_dir,
+            state,
+            num_checkpoints_to_keep,
+        ),
     )
     signal.signal(
         signal.SIGTERM,
-        lambda sig, frame: signal_handler(sig, frame, preemption_log_path,
-            checkpoint_dir, state, num_checkpoints_to_keep)
+        lambda sig, frame: signal_handler(
+            sig,
+            frame,
+            preemption_log_path,
+            checkpoint_dir,
+            state,
+            num_checkpoints_to_keep,
+        ),
     )
 
-def check_for_preempted_run(save_location, start_date, cfg,
-    prepare_to_resume_after_timeout):
+
+def check_for_preempted_run(
+    save_location, start_date, cfg, prepare_to_resume_after_timeout
+):
     print("Bookkeeping: checking for preempted run")
 
-    # checks the runs in the start_date folder for ones which have the same 
+    # checks the runs in the start_date folder for ones which have the same
     # config and also were preemppted. Then picks the most recent one or none
     check_dir = Path(save_location).joinpath(start_date)
-    inner_run_paths = sorted(glob.glob(check_dir.as_posix() + '/*'))
+    inner_run_paths = sorted(glob.glob(check_dir.as_posix() + "/*"))
     for inner_run_path in reversed(inner_run_paths):
         inner_run_path = Path(inner_run_path)
 
         # if the log doesn't exist then it is likely we are just starting
-        # a parallel run and the other tasks haven't created it yet. so 
+        # a parallel run and the other tasks haven't created it yet. so
         # no need to preempt them
-        if not os.path.isfile(inner_run_path.joinpath('preemption_log.txt')):
+        if not os.path.isfile(inner_run_path.joinpath("preemption_log.txt")):
             continue
 
-        with open(inner_run_path.joinpath('preemption_log.txt'), 'r') as f:
+        with open(inner_run_path.joinpath("preemption_log.txt"), "r") as f:
             preemption_log = f.readlines()
-        if not ('SIGCONT' in preemption_log[-1] or 'Expecting Timeout' in preemption_log[-1]):
+        if not (
+            "SIGCONT" in preemption_log[-1] or "Expecting Timeout" in preemption_log[-1]
+        ):
             continue
-        
-        # its confusing if there's changes in configs going on as well
-        assert not os.path.isfile(inner_run_path.joinpath('config').joinpath('config_002.yaml')) 
 
-        run_config = load_ml_collections(inner_run_path.joinpath('config').joinpath('config_001.yaml'))
+        # its confusing if there's changes in configs going on as well
+        assert not os.path.isfile(
+            inner_run_path.joinpath("config").joinpath("config_002.yaml")
+        )
+
+        run_config = load_ml_collections(
+            inner_run_path.joinpath("config").joinpath("config_001.yaml")
+        )
         if not run_config == cfg:
             continue
 
-        time_string = datetime.today().strftime(r'%Y-%m-%d:%H-%M-%S')
-        with open(inner_run_path.joinpath('preemption_log.txt'), 'a') as f:
+        time_string = datetime.today().strftime(r"%Y-%m-%d:%H-%M-%S")
+        with open(inner_run_path.joinpath("preemption_log.txt"), "a") as f:
             f.write(time_string + "  resuming after preemption\n")
             if prepare_to_resume_after_timeout:
                 f.write(time_string + " Expecting Timeout\n")
@@ -270,10 +331,64 @@ def check_for_preempted_run(save_location, start_date, cfg,
 
     return Path("null")
 
+
 def no_more_preemption_recovery_needed(experiment_dir):
     print("Bookkeeping: adding to preemption log that run finished")
-    preemption_log_path = experiment_dir.joinpath('preemption_log.txt')
-    time_string = datetime.today().strftime(r'%Y-%m-%d:%H-%M-%S')
-    with open(preemption_log_path, 'a') as f:
-        f.write(time_string + '  Finished Run')
+    preemption_log_path = experiment_dir.joinpath("preemption_log.txt")
+    time_string = datetime.today().strftime(r"%Y-%m-%d:%H-%M-%S")
+    with open(preemption_log_path, "a") as f:
+        f.write(time_string + "  Finished Run")
 
+
+def save_state(state: dict, save_dir) -> None:
+    # /Users/paulheller/PythonRepositories/Master-Thesis/ContTimeDiscreteSpace/tauLDR/SavedModels/MNIST
+    current_date = datetime.now().strftime("%Y-%m-%d")
+    model_filename = f"model_{state['n_iter']}.pt"
+
+    model_path = os.path.join(save_dir, current_date)
+    if not os.path.exists(model_path):
+        os.makedirs(model_path)
+    model_path = os.path.join(model_path, model_filename)
+
+    checkpoint_dict = {
+        "model": state["model"].state_dict(),
+        "optimizer": state["optimizer"].state_dict(),
+        "epoch": state["n_iter"],
+        # "ema_model_state": self.ema_model.state_dict() if self.use_ema else None,
+    }
+    torch.save(checkpoint_dict, model_path)
+
+
+def load_state(state: dict, checkpoint_path: str) -> dict:
+    checkpoint = torch.load(checkpoint_path, map_location=torch.device("cpu"))
+    state["model"].load_state_dict(checkpoint["model"])
+
+    state["optimizer"].load_state_dict(checkpoint["optimizer"])
+
+    state["n_iter"] = checkpoint["n_iter"]
+
+    return state
+
+
+def save_config(config: dict, config_dir: str) -> None:
+    # /Users/paulheller/PythonRepositories/Master-Thesis/ContTimeDiscreteSpace/tauLDR/SavedModels/MNIST
+    current_date = datetime.now().strftime("%Y-%m-%d")
+    file_name = 'config_001.yaml'
+    
+    config_dir = os.path.join(config_dir, current_date)
+    if not os.path.exists(config_dir):
+        os.makedirs(config_dir)
+
+    config_dir = os.path.join(config_dir, file_name)
+
+    yaml = ruamel.yaml.YAML()
+    with open(config_dir, "w") as yaml_file:
+        yaml.dump(config.to_dict(), yaml_file)
+
+
+def load_config(config_dir: str):
+    yaml = ruamel.yaml.YAML()
+    with open(config_dir, "r") as yaml_file:
+        config = yaml.load(yaml_file)
+
+    return config_dict.ConfigDict(config)
