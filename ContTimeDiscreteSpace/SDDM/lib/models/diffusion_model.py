d4468056e9d525e73f5a6de9d482e606e4e7edeb
Paul Heller <paulleonardoheller@yahoo.de>
2023-10-08 09:23:58 +0200

    no ema

--- ContTimeDiscreteSpace/SDDM/lib/models/diffusion_model.py
+++ ContTimeDiscreteSpace/SDDM/lib/models/diffusion_model.py
@@ -114,22 +114,22 @@ class CategoricalDiffusionModel:
         def sample_body_fn(step, xt):
             t = ones * tau * (self.config.sampling_steps - step)
             local_rng = jax.random.fold_in(rng, step)
-            new_y = self.sample_step(state.ema_params, local_rng, tau, xt, t)
+            #new_y = self.sample_step(state.ema_params, local_rng, tau, xt, t)
             #print("sample step")
-            #new_y = self.sample_step(state.params, local_rng, tau, xt, t)
+            new_y = self.sample_step(state.params, local_rng, tau, xt, t)
             return new_y
 
         def sample_with_correct_body_fn(step, xt):
             t = ones * tau * (self.config.sampling_steps - step)
             local_rng = jax.random.fold_in(rng, step)
-            xt = self.sample_step(state.ema_params, local_rng, tau, xt, t)
-            #xt = self.sample_step(state.params, local_rng, tau, xt, t)
+            #xt = self.sample_step(state.ema_params, local_rng, tau, xt, t)
+            xt = self.sample_step(state.params, local_rng, tau, xt, t)
             scale = self.config.get("corrector_scale", 1.0)
 
             def corrector_body_fn(cstep, cxt):
                 c_rng = jax.random.fold_in(local_rng, cstep)
-                cxt = self.corrector_step(state.ema_params, c_rng, tau * scale, cxt, t)
-                #cxt = self.corrector_step(state.params, c_rng, tau * scale, cxt, t)
+                #cxt = self.corrector_step(state.ema_params, c_rng, tau * scale, cxt, t)
+                cxt = self.corrector_step(state.params, c_rng, tau * scale, cxt, t)
                 return cxt
 
             new_y = jax.lax.fori_loop(
